3a4bbebfcc5de22c1e8a75f53fa91eb7d65fc60d
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2020-11-11 19:51:26 +0400

    реализовал контроллер

--- PlatfozaTestTask.API/Controllers/UserController.cs
+++ PlatfozaTestTask.API/Controllers/UserController.cs
@@ -1,10 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NHibernate.Linq;
+using PlatfozaTestTask.API.Helpers;
+using PlatfozaTestTask.API.Models;
+using PlatfozaTestTask.API.Models.DTO;
+using PlatfozaTestTask.API.Repositorues;
+using PlatfozaTestTask.API.Services;
 
 namespace PlatfozaTestTask.API.Controllers
 {
     [Route("[controller]")]
     public class UserController : Controller
     {
+        private readonly ILogger<UserController> _logger;
+        private readonly IRepository<User> _userRepository;
+        private readonly IRepository<Account> _accountRepository;
+        private readonly Mapper _mapper;
+        private readonly IIdentityService _identityService;
+
+        public UserController(ILogger<UserController> logger, 
+            IRepository<User> userRepository,
+            IRepository<Account> accountRepository,
+            IAutomapperHelper automapperHelper, 
+            IIdentityService identityService)
+        {
+            _logger = logger;
+            _userRepository = userRepository;
+            _accountRepository = accountRepository;
+            _identityService = identityService;
+            _mapper = automapperHelper.GetAutomapper();
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] AccountDTO accountDto)
+        {
+            if (accountDto == null)
+            {
+                _logger.LogError("The input data is null");
+                return BadRequest("The input data is null");
+            }
+
+            var authResult = await _identityService.AuthenticationUserAsunc(accountDto.Login, accountDto.Password, _accountRepository);
+            if (authResult.IsSuccess)
+                return Ok(authResult);
+            return Unauthorized();
+
+        }
+
+        [Authorize]
+        [Route("userinfo")]
+        [HttpGet]
+        public async Task<IActionResult> GetUserinfo()
+        {
+            var username = HttpContext.User.Identity.Name;
+            var user = await _userRepository.Get().FirstOrDefaultAsync(u => u.Name == username);
+            if (user == null) return BadRequest();
+            var userDto = _mapper.Map<User, UserDTO>(user);
+            return Ok(userDto);
+
+        }
         
+        [Route("exception")]
+        [HttpGet]
+        public IActionResult GetException() => 
+            throw new Exception("unhandled exception");
+        
+        [Route("error")]
+        [HttpGet]
+        public IActionResult GetError() =>
+            BadRequest("The requested user is not active");
     }
 }
--- PlatfozaTestTask.API/Startup.cs
+++ PlatfozaTestTask.API/Startup.cs
@@ -1,10 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using PlatfozaTestTask.API.Helpers;
 using PlatfozaTestTask.API.Middlewares;
+using PlatfozaTestTask.API.Options;
 using PlatfozaTestTask.API.Providers;
 using PlatfozaTestTask.API.Services;
 
@@ -16,6 +18,33 @@ namespace PlatfozaTestTask.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // укзывает, будет ли валидироваться издатель при валидации токена
+                        ValidateIssuer = true,
+                        // строка, представляющая издателя
+                        ValidIssuer = AuthOptions.ISSUER,
+ 
+                        // будет ли валидироваться потребитель токена
+                        ValidateAudience = true,
+                        // установка потребителя токена
+                        ValidAudience = AuthOptions.AUDIENCE,
+                        // будет ли валидироваться время существования
+                        ValidateLifetime = true,
+ 
+                        // установка ключа безопасности
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                        // валидация ключа безопасности
+                        ValidateIssuerSigningKey = true,
+                    };
+                });
+            
             services.AddSingleton();
             services.AddScoped();
             services.AddHelpers();
@@ -41,10 +70,13 @@ namespace PlatfozaTestTask.API
             }
 
             app.UseRouting();
+            
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapControllers();
             });
         }
     }
